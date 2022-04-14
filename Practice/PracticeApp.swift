3e637342292054b8b2671d4ee6e0b5265745e503
Kha Tran <khatran961217@gmail.com>
2022-04-14 14:52:13 +0700

    Initial Commit

---
+++ Practice/PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeApp.swift
+//  Practice
+//
+//  Created by Kha Tran on 14/4/2022.
+//
+
+import SwiftUI
+
+@main
+struct PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
